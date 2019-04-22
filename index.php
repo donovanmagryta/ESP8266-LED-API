14cdfd79f7929febd46d277f30529c1c60e4ff53
Donovan <triops124@gmail.com>
2019-04-22 16:39:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+ if(isset($_POST["lednum"])) { 
+$lednum = $_POST["lednum"];
+$lednum = ($lednum - 1);
+$url = $_POST["url"];
+$find = $_POST["find"];
+$calcdo = $_POST["matches"];
+ $file = fopen("program.json","a+") or die ("file not found"); 
+$json = file_get_contents('program.json');
+$data = json_decode($json, true); 
+$data[$lednum][$url] = $url;
+$data[$lednum][$find]=$find;
+$data[$lednum][$calcdo]=$calcdo;
+$newjson = json_encode($data); file_put_contents('program.json', $newjson); fclose($file);
+$message = ( $lednum . $url . $find . $calcdo) ;
+}
+else {
+echo '<html> <body> <form action="index.php" method="post"> <?php echo $message; ?> <input type="text" name="lednum" value="led number"/><input type ="text" name="url" value="api url"/><input type ="text" name="find" value="search keyword"/><input type="text" name="matches" value="must match"/><input type="submit" name="SubmitButton"/> </form> </body> </html>';
+}
+?>
+
+
+
