f323b878147483a3d4ea7edcada0359e15e21622
Donovan <triops124@gmail.com>
2019-04-24 16:05:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php 
- if(isset($_POST["lednum"])) { 
+<?php
+ if (admin == "password123") {
+ if(isset($_POST["lednum"])){
 $lednum = $_POST["lednum"];
 $lednum = ($lednum - 1);
 $url = $_POST["url"];
@@ -15,7 +16,11 @@ $newjson = json_encode($data); file_put_contents('program.json', $newjson); fclo
 $message = ( $lednum . $url . $find . $calcdo) ;
 }
 else {
-echo '<html> <body> <form action="index.php" method="post"> <?php echo $message; ?> <input type="text" name="lednum" value="led number"/><input type ="text" name="url" value="api url"/><input type ="text" name="find" value="search keyword"/><input type="text" name="matches" value="must match"/><input type="submit" name="SubmitButton"/> </form> </body> </html>';
+echo '<html> <body> <form action="index.php?admin=password123" method="post"> <?php echo $message; ?> <input type="text" name="lednum" value="led number"/><input type ="text" name="url" value="api url"/><input type ="text" name="find" value="search keyword"/><input type="text" name="matches" value="must match"/><input type="submit" name="SubmitButton"/> </form> </body> </html>';
+}
+}
+ else {
+ echo $messagejson;
 }
 ?>
 
