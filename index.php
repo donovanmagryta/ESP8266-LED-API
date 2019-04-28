f03de35f7066595655567f2f0be16613696f80c9
Donovan <triops124@gmail.com>
2019-04-28 13:40:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $lednum = ($lednum - 1);
 $url = $_POST["url"];
 $find = $_POST["find"];
 $calcdo = $_POST["matches"];
- $file = fopen("program.json","a+") or die ("file not found"); 
+$file = fopen("program.json","a+") or die ("file not found"); 
 $json = file_get_contents('program.json');
 $data = json_decode($json, true); 
 $data[$lednum][0] = $url;
@@ -18,7 +18,7 @@ file_put_contents('program.json', $newjson);
 fclose($file);
 $message = ( $lednum . $url . $find . $calcdo) ;
 }
-else {
+else if (!isset($_POST["lednum"])) {
 echo '<html> <body> <form action="index.php?admin=password123" method="post"> <?php echo $message; ?> <input type="text" name="lednum" value="led number"/><input type ="text" name="url" value="api url"/><input type ="text" name="find" value="search keyword"/><input type="text" name="matches" value="must match"/><input type="submit" name="SubmitButton"/> </form> </body> </html>';
 }
 }
